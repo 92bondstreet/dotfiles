2771067624855b84dbb790a0772c76a50162080e
Yassine AZZOUT <92bondstreet@gmail.com>
2014-10-21 23:40:07 +0200

    Atom packages list to install

---
+++ atom/packages.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+apm install atom-beautify;
+apm install autocomplete-plus;
+apm install editorconfig;
+apm install file-icons;
+apm install git-plus;
+apm install git-tab-status;
+apm install linter;
+apm install linter-eslint;
+apm install merge-conflicts;
+apm install minimap;
